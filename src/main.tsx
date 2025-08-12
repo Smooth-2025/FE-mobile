39bc203d2a8c6e3d86351dd8e1d2e2b9b264eb9c
suin <73993670+whl5105@users.noreply.github.com>
2025-08-12 13:45:41 +0900

    feat. reset css 추가 #17

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,15 @@ import { theme } from '@styles/theme.ts';
 import AppRouter from '@routes/index';
 import { store } from '@store/index.ts';
 import ViewportGate from './layout/ViewportGate';
+import { GlobalStyle } from './styles/globalStyle';
+import '@styles/reset.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <ViewportGate>
+          <GlobalStyle />
           <AppRouter />
         </ViewportGate>
       </Provider>
