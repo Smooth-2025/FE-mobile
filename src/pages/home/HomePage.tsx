31a5649351c69c50c65492424d8057f09be35fdc
suin <73993670+whl5105@users.noreply.github.com>
2025-08-14 13:07:58 +0900

    fix: 페이지 컴포넌트  선언 변경

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,7 @@
 import Header from '@/layout/Header';
 import { theme } from '@/styles/theme';
 
-export default function homePage() {
+export default function HomePage() {
   return (
     <main>
       <Header type="logo" bgColor={theme.colors.bg_page} />
