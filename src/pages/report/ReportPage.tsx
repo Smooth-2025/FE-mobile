441190b6dc911b283ac6fecf2dbfd69bf74f399b
suin <73993670+whl5105@users.noreply.github.com>
2025-08-24 22:37:30 +0900

    feat: 리포트 페이지 header 적용

--- src/pages/report/ReportPage.tsx
+++ src/pages/report/ReportPage.tsx
@@ -1,8 +1,11 @@
 import StampBoard from '@/components/report/StampBoard';
+import Header from '@/layout/Header';
+import { theme } from '@/styles/theme';
 
 export default function ReportPage() {
   return (
     <main>
+      <Header type="logo" bgColor={theme.colors.bg_page} />
       <StampBoard />
     </main>
   );
