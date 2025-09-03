af4b89d2cc04c88394c8d96ee174c3b8d2029949
suin <73993670+whl5105@users.noreply.github.com>
2025-09-03 19:25:12 +0900

    style: 불필요한 파일 삭제

--- src/components/simulation/SimulationGround.tsx
+++
@@ -1,8 +0,0 @@
-export default function SimulationGround() {
-  return (
-    <mesh position={[0, -0.05, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[200, 200]} />
-      <meshBasicMaterial color="#2a2a2a" toneMapped={false} />
-    </mesh>
-  );
-}
