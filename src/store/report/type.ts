376037ec7323ce36a3465588b2987bcdbbea0068
suin <73993670+whl5105@users.noreply.github.com>
2025-09-04 13:31:35 +0900

    fix: 주행 현황 조회(n/15)  API 엔드포인트 URL , res data 수정

--- src/store/report/type.ts
+++ src/store/report/type.ts
@@ -1,9 +1,5 @@
 //주행 현황 조회(n/15)
 export type DrivingProgress = {
-  userId: number;
-  cycleNo: string; // 주행 리포트 갯수
-  totalTrips: number; // 사용자의 전체 주행건수
-  currentCycleCount: number; //현재 스탬프 개수 (2)
-  threshold: number; //임계치(15)
-  remainingTrips: number; // 남은 주행 기록 (15-2)
+  reportId: number;
+  numberOfDriving: number; // 현재 스탬프
 };
