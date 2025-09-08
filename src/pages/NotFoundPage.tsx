e5619bbd2357b7217dcfb39006ef5f9a98700ed7
suin <73993670+whl5105@users.noreply.github.com>
2025-09-08 16:21:11 +0900

    feat: 404 NotFound 페이지 추가

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,15 @@
-export default function notFoundPage() {
+import * as Styled from './NotFoundPage.style';
+
+export default function NotFoundPage() {
   return (
-    <main>
-      <h1>NotFoundPage</h1>
-    </main>
+    <Styled.Main>
+      <Styled.Column>
+        <Styled.Title>404 NotFound</Styled.Title>
+        <Styled.Description>페이지를 찾을 수 없습니다</Styled.Description>
+        <Styled.BackButton onClick={() => (window.location.href = '/')}>
+          메인으로 돌아가기
+        </Styled.BackButton>
+      </Styled.Column>
+    </Styled.Main>
   );
 }
